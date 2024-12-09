4b301b225190c0bea9d227dd979e98e87bd7507c
eriperi08 <ericnedre@gmail.com>
2024-12-09 09:35:55 +0100

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,1 +1,16 @@
+#include <Adafruit_NeoPixel.h>
 
+void setup() {
+  strip.begin();
+  strip.show(); // Slå av alle LED-er til å begynne med
+}
+
+void loop() {
+  //Scoreboard
+  visGoal_1(Goal_1);  // Velg tallet som skal vises
+  strip.show(); // Vis tallet på 7-segment displayet
+
+  visGoal_2(Goal_2);
+  strip.show();
+  
+}
