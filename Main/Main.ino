b1af6ff6465c7f6d6dbb92cff4ecf3c8cee9802f
eriperi08 <ericnedre@gmail.com>
2024-12-09 09:40:12 +0100

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,13 @@
 #include <Adafruit_NeoPixel.h>
 
+// Definer antall LED-piksler
+#define NUMPIXELS 14
+#define PIN 23
+int Goal_1 = 5;
+int Goal_2 = 4;
+
+Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   strip.begin();
   strip.show(); // Slå av alle LED-er til å begynne med
