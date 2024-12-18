056727fe9cdd13c57f56abbcf44355a344970306
eriperi08 <ericnedre@gmail.com>
2024-12-18 11:14:42 +0100

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -11,6 +11,18 @@ Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   strip.begin();
   strip.show(); // Slå av alle LED-er til å begynne med
+    // bestemmer hastigheten på signalet
+  Serial.begin (115200);
+  // bestemmer hvor ESP32 skal ta imot signal for begge lagene
+  pinMode(Leser_For_Lag_1, INPUT);
+  pinMode(Leser_For_Lag_2, INPUT);
+  
+  strip1.begin();          
+  strip1.show();            
+  strip1.setBrightness(BRIGHTNESS); 
+  strip2.begin();          
+  strip2.show();            
+  strip2.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
